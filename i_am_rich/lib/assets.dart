fec700e61b96fc68342962d27971e7f54d5b52bf
Daniel clas <dclas@santandertecnologia.com.ar>
2020-08-14 21:32:41 -0300

    Initial commit

---
+++ i_am_rich/lib/assets.dart
@@ -0,0 +1,10 @@
+library assets;
+
+class Assets {
+  String image =
+      'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_views_of_the_sun5/22136942-2-eng-GB/Solar_Orbiter_s_first_views_of_the_Sun_pillars.gif';
+
+  String getImage() {
+    return this.image;
+  }
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'assets.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          centerTitle: true,
+          backgroundColor: Colors.amberAccent[400],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Center(
+            child: Image(
+              image: NetworkImage(new Assets().getImage()),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
